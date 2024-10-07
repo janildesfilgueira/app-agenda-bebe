2a136cc7ce17d682951d7a1698b9862e423320d1
janildesfilgueira <janildesfilgueira@gmail.com>
2024-10-07 14:42:00 -0300

    commit do componentes DadosCrianca

---
+++ app-agenda-bebe/componentes/DadosCrianca/ViewDadosCrianca.js
@@ -0,0 +1,13 @@
+import { Button,Text,View } from "react-native";
+
+const ViewDadosCrianca = () => {
+    return (
+        <View>
+            <Text>Hora</Text>
+            <Text>Nome da Atividade</Text>
+            <Button>Salvar</Button>
+        </View>
+    )
+}
+
+export default ViewDadosCrianca;
